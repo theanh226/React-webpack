91078976e75cd53f136da29739c6acaaa38b3114
theanh <theanh@dev2.edunet24.de>
2018-08-14 15:13:21 +0200

    use if else and loop in Reactjs

--- src/components/App/Products.js
+++ src/components/App/Products.js
@@ -2,15 +2,59 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../style.css';
 
+//function 
+
+
 class Products extends Component {
+    showInfoProduct(product){
+        if(product.status){
+
+            return  (
+                <div>
+                    <h3>Id: {product.id}</h3> 
+                    <h3>Name: {product.name}</h3>
+                    <h3>Price: {product.price} $</h3>
+                    <h3>Status: {product.status ? 'Active':'Deactive'} </h3>
+                </div>
+            )
+    
+        }
+    };
+
+    
     render(){
+        var users = [
+        {
+            id:1,
+            name:'The anh tran',
+            age:23
+        },
+        {
+            id:2,
+            name:'nguyen nhu mai',
+            age:23
+        },
+        {
+            id:3,
+            name:'The ngoc anh',
+            age:11
+        }
+    ];
+
+    var elemenst = users.map( (users,index) => {
+        return <div key={index}>
+                    <h2> name : {users.name}</h2>
+                    <h2> name : {users.age}</h2>
+                </div>
+    });
         var a = 5 ;
         var testString = 'The Anh Tran';
         var b = 10;
         var productsMobile = {
              name : 'Iphone 7',
              price : 700,
-             id:1
+             id:1,
+             status : false
         }
         return(
             <div>
@@ -20,10 +64,10 @@ class Products extends Component {
                 <h3> this is variable testString : {testString} </h3>
                 <h4> Result a + b = {a + b }</h4>
 
-                <div className="showProducts">
-                      <h3>Id: {productsMobile.id}</h3> 
-                      <h3>Name: {productsMobile.name}</h3>
-                      <h3>Price: {productsMobile.price} $</h3>
+                <div className="showProducts p-1">
+                {this.showInfoProduct(productsMobile)}
+                 <br />
+                 {elemenst}
                 </div>
             </div>
         );
